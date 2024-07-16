c2cacfea1b13f6b4d51bc3b2218a704764cff1f1
AbdoG1 <abdogafar17@gmail.com>
2024-07-16 13:58:40 +0300

    all pages nav+footer done

--- src/js/index.js
+++ src/js/index.js
@@ -4,5 +4,29 @@ import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
 import 'jquery/dist/jquery.min.js';
 import '@popperjs/core';
 import '../sass/style.scss';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 
-console.log("hello world");
+
+
+document.getElementById('date').appendChild(document.createTextNode(new Date().getFullYear()));
+
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,59 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
+const loader = require("sass-loader");
 
 module.exports = {
 
     entry: './src/js/index.js',
 
     output: {
+        publicPath: '/',
         path: path.resolve(__dirname, 'build'),
         filename: 'js/bundle.js',
       },
 
       module: {
         rules: [
+
           {
             test: /\.(sass|css|scss)$/,
             use: [
               // Creates `style` nodes from JS strings
-              MiniCssExtractPlugin.loader,
+             {
+                loader:  MiniCssExtractPlugin.loader,
+                options: {
+                    publicPath: '../'
+                }
+             },
               // Translates CSS into CommonJS
               "css-loader",
               // Compiles Sass to CSS
               "sass-loader",
             ],
           },
+
+          {
+
+            test: /\.(png|jpe?g|gif)$/i,
+
+            use: [
+
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: "images",
+                }
+              },
+            ]
+          },
+
+          {
+            test: /\.html$/i,
+            loader: 'html-loader',
+          },
+
         ],
       },
 
@@ -41,6 +71,22 @@ module.exports = {
         template: './src/index.html',
         filename: 'index.html'
         }),
+
+        new HtmlWebpackPlugin({
+          template: './src/distributors.html',
+          filename: 'distributors.html'
+          }),
+
+        new HtmlWebpackPlugin({
+          template: './src/about.html',
+          filename: 'about.html'
+          }),
+
+        new HtmlWebpackPlugin({
+          template: './src/contact.html',
+          filename: 'contact.html'
+          }),
+      
 
         new MiniCssExtractPlugin()
     ],
