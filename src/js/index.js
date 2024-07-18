e1b43ab09da0ffadb7b3f954e908a6a442d3aa89
AbdoG1 <abdogafar17@gmail.com>
2024-07-18 17:16:34 +0300

    all pages done

--- src/js/index.js
+++ src/js/index.js
@@ -9,9 +9,23 @@ import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 
-
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+      thisArg = thisArg || window;
+      for (var i = 0; i < this.length; i++) {
+          callback.call(thisArg, this[i], i, this);
+      }
+  };
+}
 
 document.getElementById('date').appendChild(document.createTextNode(new Date().getFullYear()));
+
+document.querySelectorAll('.add-btn').forEach(item => {
+  item.addEventListener('click', () => {
+      alert('اضيف المنتج الى عربة الشراء')
+  })
+})
+
 
 (function() {
     'use strict';
